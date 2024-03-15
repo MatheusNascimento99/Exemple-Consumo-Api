1576a1e25349afc0aca90bc0fbb1c23fa2cba385
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-15 11:58:46 -0300

    ajuste responsividade page login

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -5,23 +5,32 @@ import UserList from "./userList";
 import Menu from '../../assets/menu.png';
 import Bell from '../../assets/bell.png';
 import Profile from '../../assets/profile.png';
+import RArrow from '../../assets/right-arrow.png';
 
 const UserPage = () => {
     return (
         <main className='UserPageFull'>
             <div className='UserPageBar'>
                 <div className='UserPageTxt'>
-                    <p>SUPPLAY CHAIN</p> <Link href={''}><Image className='MenuEmBarra' src={Menu} alt='Icone menu em barra' /></Link> 
+                    <p>SUPPLAY CHAIN</p> <Link href={''}><Image className='MenuEmBarra' src={Menu} alt='Icone menu em barra' /></Link>
                 </div>
 
-
                 <div className='UserPageIcon'>
-                   <Link href={''}><Image className='Bell' src={Bell} alt='Icone sino' /></Link> 
-                   <Link href={''}><Image className='Profile' src={Profile} alt='Icone Profile' /></Link>  
+                    <Link href={''}><Image className='Bell' src={Bell} alt='Icone sino' /></Link>
+                    <Link href={''}><Image className='Profile' src={Profile} alt='Icone Profile' /></Link>
                 </div>
 
             </div>
-            <UserList></UserList>
+            <div className='MenuUserList'>
+                <div className='UserSideBar'>
+                    <p>USUÁRIOS</p> <Link href={''}><Image className='RArrow' src={RArrow} alt='Icone RArrow' /></Link>
+                </div>
+                <div className='UserList'>
+                    {/* <UserList></UserList> */}
+
+                </div>
+
+            </div>
         </main>
     )
 
